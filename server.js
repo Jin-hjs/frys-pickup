679c0739409215eb523fc510ad8a0129a821e69c
mello-code <48005787+mello-code@users.noreply.github.com>
2021-11-30 17:12:23 -0700

    feat: initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express'),
+    path = require("path");
+
+const app = express(),
+    port = process.env.PORT || 8080;
+
+app.use(express.static(__dirname + './public'));
+
+app.use("/", (req, res) => {
+    // res.send(`port is ${port}`);
+    res.sendFile(path.join(__dirname + '/public/landing.html'));
+});
+
+app.listen(port);
