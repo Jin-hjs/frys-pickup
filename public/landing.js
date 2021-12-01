6cf97a95caf283d02a5258bb938943e6501fea2e
mello-code <48005787+mello-code@users.noreply.github.com>
2021-11-30 20:06:17 -0700

    feat: everything

---
+++ public/landing.js
@@ -0,0 +1,53 @@
+const staginglocationTexts = document.getElementsByClassName('stage'),
+    hourTexts = document.getElementsByClassName('time'),
+    getCurrentTime = () => {
+        const time = new Date(),
+            hour = time.getHours() + ~~(time.getMinutes() / 30);
+
+        // very sophisticated, I know.
+
+        if (hour < 6) {
+            return 8;
+        } else if (hour < 8) {
+            return 10;
+        } else if (hour < 10) {
+            return 12;
+        } else if (hour < 12) {
+            return 14;
+        } else if (hour < 14) {
+            return 16;
+        } else if (hour < 16) {
+            return 18;
+        }
+
+        return 6;
+    },
+    getStagingLocation = hour => {
+        const remainder = hour % 4;
+
+        switch (remainder) {
+            case 0:
+                return 'D';
+            case 1:
+                return 'A';
+            case 2:
+                return 'C';
+            case 3:
+                return 'B';
+        }
+    },
+    formatHour = hour => {
+        return hour >= 12 ? hour + 'PM' : hour + 'AM';
+    },
+    updatePage = () => {
+        const selectedTime = getCurrentTime();
+
+        staginglocationTexts[0].innerHTML = `<span class="inline yellow">${formatHour(selectedTime)}</span>s`;
+        hourTexts[0].innerHTML = `to <span class="inline yellow">${getStagingLocation(selectedTime)}</span>`;
+
+        staginglocationTexts[1].innerHTML = `<span class="inline orange">${formatHour(selectedTime + 1)}</span>s`;
+        hourTexts[1].innerHTML = `to <span class="inline orange">${getStagingLocation(selectedTime + 1)}</span>`;
+    };
+
+updatePage();
+setInterval(updatePage, 1000);
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express'),
 const app = express(),
     port = process.env.PORT || 8080;
 
-app.use(express.static(__dirname + './public'));
+app.use(express.static(__dirname + '/public'));
 
 app.use("/", (req, res) => {
     // res.send(`port is ${port}`);
